91c0bd91da8f5b0ed3113612270434fda54aa4ce
Егор <egr.bar152@gmail.com>
2018-06-01 14:08:47 +0700

    Add files via upload

---
+++ Assets/Scripts/PlayerCl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCl : MonoBehaviour
+{
+
+    public float speed = 4f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(new Vector3(moveX, moveY, 0));
+    }
+}
